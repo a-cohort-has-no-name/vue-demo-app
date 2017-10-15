9a9cfddca0e80dc99ed9bf805a582f93628765fc
danizaghian <dani.zaghian@gmail.com>
2017-10-15 11:54:11 -0700

    solutions

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,16 +1,26 @@
 /* global Vue */
 document.addEventListener("DOMContentLoaded", function(event) { 
-  var app = new Vue({
-    el: '#tasks',
+  var task = new Vue({
+    el: '#task',
     data: {
       message: 'Hello Vue!',
-      tasks: ['Take out the trash', 'Feed the cat', 'Mow the lawn']
+      tasks: ['Take out the trash', 'Feed the cat', 'Mow the lawn'],
+      newTask: ''
     },
     mounted: function() {
 
     },
     methods: {
-
+      addTask: function() {
+        if (this.newTask) {
+          this.tasks.push(this.newTask);
+          this.newTask = '';
+        }
+      },
+      completeTask: function(inputTask) {
+        var index = this.tasks.indexOf(inputTask);
+        this.tasks.splice(index, 1);
+      }
     },
     computed: {
 
