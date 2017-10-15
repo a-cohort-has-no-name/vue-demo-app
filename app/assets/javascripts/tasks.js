e4bbb69b04b057f9ebbe592552f3d4f001a63466
danizaghian <dani.zaghian@gmail.com>
2017-10-15 10:22:02 -0700

    v model input

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      task1: 'Take out the trash',
+      task2: 'Feed the cat',
+      task3: 'Mow the lawn'
     },
     mounted: function() {
 
